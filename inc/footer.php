779471772b2bb9daf5adaa2273000e1bfb600dc5
Tuan <nguyenquoctuan03032001@gmail.com>
2022-10-13 21:09:13 +0700

    Front-end

---
+++ inc/footer.php
@@ -0,0 +1,30 @@
+
+  <footer class="page-footer teal lighten-2">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">English Test Online</h5>
+          <p class="grey-text text-lighten-4">
+            Được xây dựng và phát triển bởi sinh viên trường Đại Học Quy Nhơn với mục tiêu hỗ trợ học tập cho các bạn học sinh sắp bước vào kì thi THPT quốc gia với bộ môn Tiếng Anh</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Contacts Us</h5>
+          <ul>
+            <li>Trần Quốc Bảo</a></li>
+            <li>Phạm Minh Dân</a></li>
+	<li>Đồng Nguyễn Hữu Phước</a></li>
+		<li>Nguyễn Quốc Tuấn</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2022 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  
+  <title></title>
+</head> 
+
+<body>
+  <div class="header">
+    <div class='console-container'><span id='text'></span>
+      <div class='console-underscore' id='console'></div>
+    </div>
+
+  </div>
+<script>
+
+</script>
+
