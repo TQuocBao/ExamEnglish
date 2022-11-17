102573224b636694c68bf4ff01215ade35a49999
Dan <minhdanqn25@gmail.com>
2022-11-17 08:06:45 +0700

    index

--- admin/index.php
+++ admin/index.php
@@ -46,3 +46,23 @@
             }
             ob_end_flush();
           ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/313025c77d.js" crossorigin="anonymous"></script>
+
+    <title>Online Exam English</title>
+</head>
