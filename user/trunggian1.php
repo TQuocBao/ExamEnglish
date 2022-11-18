33668d538c541c195a59bf2cd84ca9025c1a4684
Tuan <nguyenquoctuan03032001@gmail.com>
2022-11-18 10:44:13 +0700

    Trung gian 1

--- user/trunggian1.php
+++ user/trunggian1.php
@@ -4,6 +4,7 @@
             $user = $_SESSION['users'];
         }
  ?>
+ 
 <?php 
 	require '../database/database.class.php';
 		$config = [
@@ -14,6 +15,7 @@
 		];
 		$data = new database($config);
  ?>
+
  <?php 
  		if(isset($_GET['dethi'])){
  			$made = $_GET['dethi'];
@@ -33,9 +35,28 @@
  		else
  			$ketqua = 0;
   ?>
+
  <?php 
 		date_default_timezone_set("Asia/Ho_Chi_Minh");
 		$time_now=getdate();
 		$time=$time_now['hours'].':'.$time_now['minutes'].':'.$time_now['seconds'].'';
 		$day=$time_now['mday'].'.'.$time_now['mon'].'.'.$time_now['year'];
 ?>
+
+<?php 
+	$add_history = "insert into lichsu values('".$user."','".$made."','".$socau."','".$ketqua."','".$time."','".$day."')";
+	$history = $data->ManipulationDB($add_history);
+	$rand = 0;
+	$update_rand = "update nguoidung set ran='".$rand."' where email='".$user."'";
+	$re_rand = $data->ManipulationDB($update_rand);
+ ?>
+
+<?php 
+	$delele_result = "delete from dapanchon where email ='".$user."'";
+	$r = $data->ManipulationDB($delele_result);
+	if(isset($_SESSION['dethi'])){
+		session_unset($_SESSION['dethi']);
+		$_SESSION['hetgio']=1;	
+	}
+	header('location:showDe.php?q=1');
+ ?>
